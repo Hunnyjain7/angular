d58ea64c6b4d0caffd760a9cba234f3d61501722
hunny <hunnyjain711@gmail.com>
2022-12-19 00:32:10 +0530

    angular14

---
+++ angular14/src/app/add-contact/add-contact.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.css']
+})
+export class AddContactComponent {
+
+  constructor(private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    const routeid = this.route.snapshot.paramMap.get('id')
+    console.log(routeid);
+    
+  }
+
+}
